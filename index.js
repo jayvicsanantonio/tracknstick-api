ea7dcea7ef03f7719b840099e4df2634444d887a
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-04-14 19:27:10 -0700

    fix: Fallback

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const limiter = rateLimit({
   max: process.env.RATE_LIMIT_MAX_REQUESTS || 100,
   standardHeaders: true,
   legacyHeaders: false,
-  message: `Too many requests from this IP, please try again after ${Math.round(process.env.RATE_LIMIT_WINDOW_MS / 60000)} minutes`,
+  message: `Too many requests from this IP, please try again after ${Math.round((process.env.RATE_LIMIT_WINDOW_MS || 900000) / 60000)} minutes`,
 });
 app.use('/api', limiter);
 
