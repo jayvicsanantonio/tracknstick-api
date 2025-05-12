b22fc12ee62d291f87b99c1aee4e72d0812fe690
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2025-05-12 13:33:36 -0700

    Updated CHANGELOG.md

---
+++ db/migrations/20250512184735_add_longest_streak.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('habits', (table) => {
+    table.integer('longest_streak').defaultTo(0);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('habits', (table) => {
+    table.dropColumn('longest_streak');
+  });
+};
